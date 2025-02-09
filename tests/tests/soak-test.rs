e6140c5a95b2b810addb4c9bcaa57a97f20188b7
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2025-02-09 17:46:17 +0700

    Fix soak tests

--- tests/tests/soak-test.rs
+++ tests/tests/soak-test.rs
@@ -1,131 +1,175 @@
-// #[derive(Debug, Clone, Deserialize)]
-// pub struct EnvVar {
-//     pub master_mnemonic: String,
-//     pub private_key: String,
-//     pub num_of_recipients: Option<u32>,
-//     pub recipient_offset: Option<u32>,
-//     pub balance_prover_base_url: String,
-// }
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
-// #[derive(Debug, Default, Clone, Copy)]
-// struct TransactionStats {
-//     success_count: u32,
-//     failure_count: u32,
-// }
+use ethers::{types::H256, utils::hex};
+use intmax2_cli::{
+    cli::{
+        error::CliError,
+        send::{transfer, TransferInput},
+    },
+    format::privkey_to_keyset,
+};
+use intmax2_zkp::{
+    common::signature::key_set::KeySet, ethereum_types::u32limb_trait::U32LimbTrait,
+};
+use serde::Deserialize;
+use tests::{derive_intmax_keys, wait_for_balance_synchronization};
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct EnvVar {
+    pub master_mnemonic: String,
+    pub private_key: String,
+    pub num_of_recipients: Option<u32>,
+    pub recipient_offset: Option<u32>,
+    pub balance_prover_base_url: String,
+    pub concurrent_limit: Option<usize>,
+    pub cool_down_seconds: Option<u64>,
+}
+
+const ETH_TOKEN_INDEX: u32 = 0;
 
 // async fn process_account(key: KeySet, transfers: &[TransferInput]) -> Result<(), CliError> {
-//     transfer(key, transfers).await?;
-//     tokio::time::sleep(Duration::from_secs(20)).await;
-//     wait_for_balance_synchronization(key, Duration::from_secs(5)).await
-// }
-
-// async fn account_loop(
-//     stats: Arc<Mutex<HashMap<String, TransactionStats>>>,
-//     semaphore: Arc<Semaphore>,
-//     key: KeySet,
-//     transfers: &[TransferInput],
-//     cool_down_seconds: u64,
-// ) {
-//     loop {
-//         if account_id < config.num_of_recipients.unwrap_or(10) {
-//             continue;
-//         }
-
-//         let permit = semaphore.clone().acquire_owned().await.unwrap();
-//         log::trace!("Starting transfer from {}", key.pubkey);
-//         let result = process_account(key, transfers).await;
-
-//         let mut stats = stats.lock().unwrap();
-//         let entry = stats
-//             .entry(key.pubkey.to_hex())
-//             .or_insert(TransactionStats::default());
-
-//         match result {
-//             Ok(_) => entry.success_count += 1,
-//             Err(_) => entry.failure_count += 1,
-//         }
-
-//         drop(permit);
-
-//         println!(
-//             "[Account {}] Cooldown: Waiting {} seconds before next transaction...\n",
-//             key.pubkey.to_hex(),
-//             cool_down_seconds
-//         );
-//         tokio::time::sleep(Duration::from_secs(cool_down_seconds)).await;
-//     }
-// }
+async fn process_account(
+    key: KeySet,
+    transfers: &[TransferInput],
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    transfer(key, transfers)
+        .await
+        .map_err(|e| anyhow::anyhow!("{e:?}"))?;
+    tokio::time::sleep(Duration::from_secs(20)).await;
+    wait_for_balance_synchronization(key, Duration::from_secs(5))
+        .await
+        .map_err(|e| e.into())
+}
 
 #[tokio::test]
 async fn test_soak_block_generation() -> Result<(), Box<dyn std::error::Error>> {
-    // dotenv::dotenv()?;
-    // dotenv::from_path("../cli/.env")?;
-    // let config = envy::from_env::<EnvVar>().unwrap();
-    // env_logger::builder()
-    //     .filter_level(log::LevelFilter::Info)
-    //     .init();
+    dotenv::dotenv()?;
+    dotenv::from_path("../cli/.env")?;
+    let config = envy::from_env::<EnvVar>().unwrap();
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
 
-    // log::debug!(
-    //     "block_builder_base_url: {:?}",
-    //     config.balance_prover_base_url
-    // );
+    let private_key = H256::from_slice(&hex::decode(config.private_key)?);
+    let admin_key = privkey_to_keyset(private_key);
 
-    // let master_mnemonic_phrase = config.master_mnemonic;
-    // let num_of_recipients = config.num_of_recipients.unwrap_or(1);
-    // log::debug!("Number of recipients: {}", num_of_recipients);
-    // if num_of_recipients > 128 {
-    //     return Err("Number of recipients must be less than or equal to 128".into());
-    // }
-
-    // let offset = 0;
-    // let intmax_senders = derive_intmax_keys(&master_mnemonic_phrase, num_of_recipients, offset)?;
-
-    // let intmax_recipient = {
-    //     let options = MnemonicToPrivateKeyOptions {
-    //         account_index: 1,
-    //         address_index: 0,
-    //     };
-    //     let private_key = mnemonic_to_private_key(&master_mnemonic_phrase, options)?;
-
-    //     generate_intmax_account_from_eth_key(private_key)
-    // };
-
-    // // multiple senders -> receiver (simultaneously)
-    // let transfer_input = TransferInput {
-    //     recipient: intmax_recipient.pubkey.to_hex(),
-    //     amount: 10u128,
-    //     token_index: ETH_TOKEN_INDEX,
-    // };
-    // let transfers = [transfer_input];
-
-    // log::info!("Transferring from recipients to sender...");
-    // tokio::time::sleep(Duration::from_secs(1)).await;
-
-    // let semaphore = Arc::new(Semaphore::new(config.concurrent_limit.unwrap_or(5)));
-    // let stats = Arc::new(Mutex::new(HashMap::new()));
-
-    // let mut tasks = vec![];
-
-    // for account_id in 0..100 {
-    //     let semaphore_clone = Arc::clone(&semaphore);
-    //     let stats_clone = Arc::clone(&stats);
-    //     let key = intmax_senders[account_id as usize];
-
-    //     let task = tokio::spawn(async move {
-    //         account_loop(
-    //             stats_clone,
-    //             semaphore_clone,
-    //             key,
-    //             &transfers,
-    //             config.cool_down_seconds.unwrap_or(30),
-    //         )
-    //         .await;
-    //     });
-
-    //     tasks.push(task);
-    // }
-
-    // join_all(tasks).await.into_iter().collect()
+    let concurrent_limit = 5;
+    let duration_secs = 60;
+    let system = TestSystem::new(admin_key, config.master_mnemonic);
+    system
+        .run_soak_test(concurrent_limit, duration_secs)
+        .await?;
 
     Ok(())
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct TestConfig {
+    concurrent_limit: usize,
+}
+
+async fn get_config() -> Result<TestConfig, Box<dyn std::error::Error>> {
+    let config = envy::from_env::<TestConfig>()?;
+    Ok(config)
+}
+
+#[derive(Debug, Clone)]
+struct TestSystem {
+    admin_key: KeySet,
+    master_mnemonic_phrase: String,
+    accounts: Arc<Mutex<Vec<KeySet>>>,
+}
+
+impl TestSystem {
+    pub fn new(admin_key: KeySet, master_mnemonic_phrase: String) -> Self {
+        Self {
+            admin_key,
+            master_mnemonic_phrase,
+            accounts: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+
+    async fn transfer_from_admin(&self, intmax_recipients: &[KeySet]) -> Result<(), CliError> {
+        let transfers = intmax_recipients
+            .iter()
+            .map(|recipient| TransferInput {
+                recipient: recipient.pubkey.to_hex(),
+                amount: 1000000000u128,
+                token_index: ETH_TOKEN_INDEX,
+            })
+            .collect::<Vec<_>>();
+        transfer(self.admin_key, &transfers).await
+    }
+
+    async fn ensure_accounts(
+        &self,
+        required_count: usize,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let accounts_len = self.accounts.lock().unwrap().len();
+
+        if accounts_len >= required_count {
+            return Ok(());
+        }
+        let num_of_keys = required_count - accounts_len;
+
+        // Create new account and receive initial balance from admin
+        let master_mnemonic_phrase = self.master_mnemonic_phrase.clone();
+        let new_accounts = derive_intmax_keys(
+            &master_mnemonic_phrase,
+            num_of_keys as u32,
+            accounts_len as u32,
+        )?;
+
+        let chunk_size = 63;
+        for chunk in new_accounts.chunks(chunk_size) {
+            self.transfer_from_admin(chunk).await?;
+            self.accounts.lock().unwrap().extend(chunk.iter());
+        }
+
+        Ok(())
+    }
+
+    pub async fn run_soak_test(
+        &self,
+        concurrent_accounts: usize,
+        duration_secs: u64,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        // Ensure we have enough accounts
+        self.ensure_accounts(concurrent_accounts).await?;
+
+        let end_time = std::time::Instant::now() + Duration::from_secs(duration_secs);
+
+        while std::time::Instant::now() < end_time {
+            let concurrent_accounts = get_config().await?.concurrent_limit;
+
+            let mut handles = vec![];
+
+            // Spawn concurrent transactions
+            for i in 0..concurrent_accounts {
+                let system = Arc::new(self.clone());
+                let account = system.accounts.lock().unwrap()[i];
+                let handle = tokio::task::spawn_local(async move {
+                    let transfers = [TransferInput {
+                        recipient: system.admin_key.pubkey.to_hex(),
+                        amount: 10u128,
+                        token_index: ETH_TOKEN_INDEX,
+                    }];
+                    if let Err(e) = process_account(account, &transfers).await {
+                        eprintln!("Transaction error: {}", e);
+                    }
+                });
+                handles.push(handle);
+            }
+
+            // Wait for all transactions to complete
+            for handle in handles {
+                handle.await?;
+            }
+        }
+
+        Ok(())
+    }
 }
--- tests/tests/stress-test.rs
+++ tests/tests/stress-test.rs
@@ -1,146 +1,20 @@
 use std::time::Duration;
 
-use ethers::{
-    signers::coins_bip39::{English, Mnemonic},
-    types::H256,
-    utils::hex,
-};
+use ethers::{types::H256, utils::hex};
 use futures::future::join_all;
 use intmax2_cli::{
-    cli::{
-        error::CliError,
-        get::balance,
-        send::{transfer, TransferInput},
-    },
+    cli::send::{transfer, TransferInput},
     format::privkey_to_keyset,
 };
-use intmax2_client_sdk::client::{
-    error::ClientError, key_from_eth::generate_intmax_account_from_eth_key,
-    strategy::error::StrategyError, sync::error::SyncError,
+use intmax2_client_sdk::client::key_from_eth::generate_intmax_account_from_eth_key;
+use intmax2_zkp::ethereum_types::u32limb_trait::U32LimbTrait;
+use tests::{
+    derive_intmax_keys, mnemonic_to_private_key, transfer_with_error_handling,
+    wait_for_balance_synchronization, EnvVar, MnemonicToPrivateKeyOptions,
 };
-use intmax2_interfaces::api::error::ServerError;
-use intmax2_zkp::{
-    common::signature::key_set::KeySet, ethereum_types::u32limb_trait::U32LimbTrait,
-};
-use serde::Deserialize;
-use tiny_hderive::bip32::ExtendedPrivKey;
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct EnvVar {
-    pub master_mnemonic: String,
-    pub private_key: String,
-    pub num_of_recipients: Option<u32>,
-    pub recipient_offset: Option<u32>,
-    pub balance_prover_base_url: String,
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct MnemonicToPrivateKeyOptions {
-    pub account_index: u32,
-    pub address_index: u32,
-}
 
 const ETH_TOKEN_INDEX: u32 = 0;
 const NUM_TRANSFER_LOOPS: usize = 2;
-
-fn mnemonic_to_private_key(
-    mnemonic_phrase: &str,
-    options: MnemonicToPrivateKeyOptions,
-) -> Result<H256, Box<dyn std::error::Error>> {
-    let mnemonic = Mnemonic::<English>::new_from_phrase(mnemonic_phrase)?;
-    let seed = mnemonic.to_seed(None)?;
-
-    let account_index = options.account_index;
-    let address_index = options.address_index;
-    let hd_path = format!("m/44'/60'/{account_index}'/0/{address_index}");
-
-    let master_key = ExtendedPrivKey::derive(&seed, hd_path.as_str())
-        .map_err(|e| format!("Failed to derive private key: {e:?}"))?;
-    let private_key_bytes = master_key.secret();
-
-    Ok(H256(private_key_bytes))
-}
-
-pub async fn wait_for_balance_synchronization(
-    intmax_sender: KeySet,
-    retry_interval: Duration,
-) -> Result<(), CliError> {
-    loop {
-        let result = balance(intmax_sender).await;
-        match result {
-            Ok(_) => return Ok(()),
-            Err(CliError::SyncError(SyncError::StrategyError(StrategyError::PendingTxError(
-                _,
-            )))) => {
-                println!("Pending transaction. Waiting for the balance to be updated...");
-            }
-            Err(CliError::SyncError(SyncError::ValidityProverIsNotSynced(_))) => {
-                println!("Validity prover is not synced. Waiting for the balance to be updated...");
-            }
-            Err(CliError::SyncError(SyncError::ServerError(ServerError::ServerError(
-                500,
-                message,
-                _,
-                _,
-            )))) => {
-                println!("{message}. Waiting for the balance to be updated...");
-            }
-            Err(CliError::ClientError(ClientError::ServerError(ServerError::ServerError(
-                status,
-                message,
-                url,
-                query,
-            )))) => {
-                println!("Server error status={status}, url={url}, query={query}");
-                println!("{message}. Waiting for the balance to be updated...");
-            }
-            Err(e) => {
-                return Err(e);
-            }
-        }
-
-        tokio::time::sleep(retry_interval).await;
-    }
-}
-
-async fn transfer_with_error_handling(
-    key: KeySet,
-    transfer_inputs: &[TransferInput],
-    num_loops: usize,
-) -> Result<(), CliError> {
-    for i in 0..num_loops {
-        log::trace!(
-            "Starting transfer from {} (iteration {}/{})",
-            key.pubkey,
-            i + 1,
-            num_loops
-        );
-        transfer(key, transfer_inputs).await?;
-        tokio::time::sleep(Duration::from_secs(20)).await;
-        wait_for_balance_synchronization(key, Duration::from_secs(5)).await?;
-    }
-
-    Ok(())
-}
-
-pub fn derive_intmax_keys(
-    master_mnemonic_phrase: &str,
-    num_of_keys: u32,
-    offset: u32,
-) -> Result<Vec<KeySet>, Box<dyn std::error::Error>> {
-    let mut intmax_senders = vec![];
-    for address_index in 0..num_of_keys {
-        let options = MnemonicToPrivateKeyOptions {
-            account_index: 0,
-            address_index: offset + address_index,
-        };
-        let private_key = mnemonic_to_private_key(master_mnemonic_phrase, options)?;
-        let key = generate_intmax_account_from_eth_key(private_key);
-        intmax_senders.push(key);
-    }
-
-    Ok(intmax_senders)
-}
 
 #[tokio::test]
 async fn test_bulk_transfers() -> Result<(), Box<dyn std::error::Error>> {
@@ -273,11 +147,9 @@ async fn test_block_generation_included_many_senders() -> Result<(), Box<dyn std
     tokio::time::sleep(Duration::from_secs(1)).await;
 
     let futures = intmax_senders.iter().map(|sender| async {
-        let future = transfer_with_error_handling(*sender, &transfers, NUM_TRANSFER_LOOPS)
+        transfer_with_error_handling(*sender, &transfers, NUM_TRANSFER_LOOPS)
             .await
-            .err();
-
-        future
+            .err()
     });
 
     let errors = join_all(futures).await;
