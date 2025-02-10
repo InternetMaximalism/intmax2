b7547c82fc4e5c7cf7b8877a8f50441b08356415
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-02-10 08:57:52 +0700

    fix compile error

--- store-vault-server/migrations/20250209134341_add_encrypted_temp.up.sql
+++ store-vault-server/migrations/20250209134341_add_encrypted_temp.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS encrypted_temp (
-    pubkey VARCHAR(66) PRIMARY KEY,
+    uuid TEXT PRIMARY KEY,
+    topic VARCHAR(66) NOT NULL,
+    pubkey VARCHAR(66) NOT NULL,
     encrypted_data BYTEA NOT NULL,
     timestamp BIGINT NOT NULL
 );
