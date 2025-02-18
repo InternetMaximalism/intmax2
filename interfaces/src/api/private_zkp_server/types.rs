511d2399a60193f4d4d905683e63877cba2556a6
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-02-18 18:47:27 +0700

    update types

--- interfaces/src/api/private_zkp_server/types.rs
+++ interfaces/src/api/private_zkp_server/types.rs
@@ -1,7 +1,8 @@
+use intmax2_zkp::ethereum_types::u256::U256;
 use serde::{Deserialize, Serialize};
 use serde_with::{base64::Base64, serde_as};
 
-use crate::data::encryption::{rsa::RsaEncryptedMessage, RsaEncryption};
+use crate::data::encryption::RsaEncryption;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -13,29 +14,48 @@ pub enum ProveType {
     ReceiveDeposit,
     SingleWithdrawal,
     SingleClaim,
+    Echo, // for testing
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ProveRequestWithType {
     pub prove_type: ProveType,
+    pub pubkey: U256,
     pub request: Vec<u8>,
 }
 
 impl RsaEncryption for ProveRequestWithType {}
 
+#[serde_as]
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct ProveRequest {
-    pub data: RsaEncryptedMessage,
+pub struct CreateProveRequest {
+    #[serde_as(as = "Base64")]
+    pub encrypted_data: Vec<u8>,
+    pub public_key: String,      // todo: remove this field
+    pub transition_type: String, // todo: remove this field
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateProofResponse {
+    request_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProofResultQuery {
+    request_id: String,
 }
 
 #[serde_as]
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct ProveResponse {
+pub struct ProofResultResponse {
+    pub status: String,
     #[serde_as(as = "Option<Base64>")]
-    pub data: Option<Vec<u8>>,
+    pub result: Option<Vec<u8>>,
     #[serde_as(as = "Option<Base64>")]
     pub error: Option<Vec<u8>>,
 }
