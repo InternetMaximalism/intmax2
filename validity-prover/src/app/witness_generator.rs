a6c2a545323bd977788458fdf91c9ee8ec80a4b7
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-02-21 13:57:52 +0700

    add batch methods to validity prover

--- validity-prover/src/app/witness_generator.rs
+++ validity-prover/src/app/witness_generator.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -334,6 +335,18 @@ impl WitnessGenerator {
         Ok(deposit_info)
     }
 
+    pub async fn get_deposit_info_batch(
+        &self,
+        deposit_hashes: &[Bytes32],
+    ) -> Result<Vec<Option<DepositInfo>>, ValidityProverError> {
+        let mut deposit_infos = Vec::new();
+        for deposit_hash in deposit_hashes {
+            let deposit_info = self.observer.get_deposit_info(*deposit_hash).await?;
+            deposit_infos.push(deposit_info);
+        }
+        Ok(deposit_infos)
+    }
+
     pub async fn get_block_number_by_tx_tree_root(
         &self,
         tx_tree_root: Bytes32,
@@ -346,6 +359,43 @@ impl WitnessGenerator {
         .await?;
 
         Ok(record.map(|r| r.block_number as u32))
+    }
+
+    pub async fn get_block_number_by_tx_tree_root_batch(
+        &self,
+        tx_tree_roots: Vec<Bytes32>,
+    ) -> Result<Vec<Option<u32>>, ValidityProverError> {
+        // early return
+        if tx_tree_roots.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        let root_bytes: Vec<Vec<u8>> = tx_tree_roots.iter().map(|r| r.to_bytes_be()).collect();
+
+        let records = sqlx::query!(
+            r#"
+            SELECT tx_tree_root, block_number 
+            FROM tx_tree_roots 
+            WHERE tx_tree_root = ANY($1)
+            "#,
+            &root_bytes as &[Vec<u8>]
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        let block_map: HashMap<Vec<u8>, i32> = records
+            .into_iter()
+            .map(|r| (r.tx_tree_root, r.block_number))
+            .collect();
+
+        Ok(tx_tree_roots
+            .iter()
+            .map(|root| {
+                block_map
+                    .get(&root.to_bytes_be())
+                    .map(|&block_number| block_number as u32)
+            })
+            .collect())
     }
 
     pub async fn get_validity_witness(
