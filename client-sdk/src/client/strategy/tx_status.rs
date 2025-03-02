3151abee9ed936dbfa3980a60ade25d8bbfeb776
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-03-02 23:50:01 +0700

    Add nonce validation testnet alpha (#191)
    * add nonce valiation
    
    * delete

---
+++ client-sdk/src/client/strategy/tx_status.rs
@@ -0,0 +1,67 @@
+use std::fmt;
+
+use intmax2_interfaces::api::validity_prover::interface::ValidityProverClientInterface;
+use intmax2_zkp::ethereum_types::{bytes32::Bytes32, u256::U256};
+use serde::{Deserialize, Serialize};
+
+use super::error::StrategyError;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub enum TxStatus {
+    Pending,
+    Success,
+    Failed(String),
+}
+
+impl fmt::Display for TxStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TxStatus::Pending => write!(f, "pending"),
+            TxStatus::Success => write!(f, "success"),
+            TxStatus::Failed(_) => write!(f, "failed"),
+        }
+    }
+}
+
+pub async fn get_tx_status<V: ValidityProverClientInterface>(
+    validity_prover: &V,
+    sender: U256,
+    tx_tree_root: Bytes32,
+) -> Result<TxStatus, StrategyError> {
+    // get onchain info
+    let block_number = validity_prover
+        .get_block_number_by_tx_tree_root(tx_tree_root)
+        .await?;
+    if block_number.is_none() {
+        return Ok(TxStatus::Pending);
+    }
+    let block_number = block_number.unwrap();
+    // sender leaves
+    let sender_leaves = validity_prover
+        .get_sender_leaves(block_number)
+        .await?
+        .ok_or(StrategyError::UnexpectedError(
+            "sender leaves not found".to_string(),
+        ))?;
+    let sender_leaf = sender_leaves.into_iter().find(|leaf| leaf.sender == sender);
+    let sender_leaf = match sender_leaf {
+        Some(leaf) => leaf,
+        None => return Ok(TxStatus::Failed("sender leaf not found".to_string())),
+    };
+    if !sender_leaf.did_return_sig {
+        return Ok(TxStatus::Failed(
+            "sender did'nt returned signature".to_string(),
+        ));
+    }
+    let validity_pis = validity_prover
+        .get_validity_pis(block_number)
+        .await?
+        .ok_or(StrategyError::UnexpectedError(
+            "validity pis not found".to_string(),
+        ))?;
+    if !validity_pis.is_valid_block {
+        return Ok(TxStatus::Failed("block is not valid".to_string()));
+    }
+    Ok(TxStatus::Success)
+}
