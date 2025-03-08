13f4395d8e501e6547965856bc201d60a234ba59
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-03-08 19:33:35 +0700

    wip

--- store-vault-server/src/app/store_vault_server.rs
+++ store-vault-server/src/app/store_vault_server.rs
@@ -1,8 +1,15 @@
 use intmax2_interfaces::{
-    api::store_vault_server::interface::SaveDataEntry, utils::digest::get_digest,
+    api::store_vault_server::{
+        interface::{SaveDataEntry, MAX_BATCH_SIZE},
+        types::{
+            CursorOrder, DataWithMetaData, DataWithTimestamp, MetaDataCursor,
+            MetaDataCursorResponse,
+        },
+    },
+    data::meta_data::MetaData,
+    utils::digest::get_digest,
 };
 use intmax2_zkp::ethereum_types::{bytes32::Bytes32, u256::U256, u32limb_trait::U32LimbTrait};
-
 use sqlx::Postgres;
 
 use server_common::db::{DbPool, DbPoolConfig};
@@ -10,14 +17,6 @@ use server_common::db::{DbPool, DbPoolConfig};
 use crate::EnvVar;
 
 use super::error::StoreVaultError;
-
-// CREATE TABLE IF NOT EXISTS historical_data (
-//     digest VARCHAR(66) PRIMARY KEY,
-//     pubkey VARCHAR(66) NOT NULL,
-//     topic VARCHAR(255) NOT NULL,
-//     data BYTEA NOT NULL,
-//     timestamp BIGINT NOT NULL
-// );
 
 type Result<T> = std::result::Result<T, StoreVaultError>;
 
@@ -113,178 +112,168 @@ impl StoreVaultServer {
         Ok(record.map(|r| (r.data, Bytes32::from_hex(&r.digest).unwrap())))
     }
 
-    pub async fn batch_save_data(&self, entries: &[SaveDataEntry]) -> Result<Vec<String>> {
+    pub async fn batch_save_data(&self, entries: &[SaveDataEntry]) -> Result<()> {
         // Prepare values for bulk insert
-        let topics: Vec<String> = entries.iter().map(|entry| entry.data_type as i32).collect();
+        let topics: Vec<String> = entries.iter().map(|entry| entry.topic.clone()).collect();
         let pubkeys: Vec<String> = entries.iter().map(|entry| entry.pubkey.to_hex()).collect();
-        let uuids: Vec<String> = (0..entries.len())
-            .map(|_| Uuid::now_v7().to_string())
-            .collect();
-        let encrypted_data: Vec<Vec<u8>> = entries
+        let digests: Vec<String> = entries
             .iter()
-            .map(|entry| entry.encrypted_data.clone())
+            .map(|entry| get_digest(&entry.data).to_hex())
             .collect();
+        let data: Vec<Vec<u8>> = entries.iter().map(|entry| entry.data.clone()).collect();
+        let timestamps = vec![chrono::Utc::now().timestamp(); entries.len()];
 
-        // Execute the bulk insert
         sqlx::query!(
             r#"
-            INSERT INTO encrypted_data
-            (data_type, pubkey, uuid, encrypted_data)
+            INSERT INTO historical_data (digest, pubkey, topic, data, timestamp)
             SELECT
-                UNNEST($1::integer[]),
+                UNNEST($1::text[]),
                 UNNEST($2::text[]),
                 UNNEST($3::text[]),
-                UNNEST($4::bytea[])
+                UNNEST($4::bytea[]),
+                UNNEST($5::bigint[])
+            ON CONFLICT (digest) DO NOTHING
             "#,
-            &data_types,
+            &digests,
             &pubkeys,
-            &uuids,
-            &encrypted_data,
+            &topics,
+            &data,
+            &timestamps
         )
         .execute(&self.pool)
         .await?;
 
-        Ok(uuids)
+        Ok(())
     }
 
-    // pub async fn get_data_batch(
-    //     &self,
-    //     data_type: DataType,
-    //     pubkey: U256,
-    //     uuids: &[String],
-    // ) -> Result<Vec<DataWithMetaData>> {
-    //     let pubkey_hex = pubkey.to_hex();
-    //     let records = sqlx::query!(
-    //         r#"
-    //         SELECT uuid, encrypted_data
-    //         FROM encrypted_data
-    //         WHERE data_type = $1 AND pubkey = $2 AND uuid = ANY($3)
-    //         "#,
-    //         data_type as i32,
-    //         pubkey_hex,
-    //         &uuids
-    //     )
-    //     .fetch_all(&self.pool)
-    //     .await?;
-    //     let result: Vec<DataWithMetaData> = records
-    //         .into_iter()
-    //         .map(|r| {
-    //             let u = Uuid::parse_str(r.uuid.as_str()).unwrap_or_default();
-    //             let meta = MetaData {
-    //                 uuid: r.uuid,
-    //                 timestamp: extract_timestamp_from_uuidv7(&u).0,
-    //             };
-    //             DataWithMetaData {
-    //                 meta,
-    //                 data: r.encrypted_data,
-    //             }
-    //         })
-    //         .collect();
-    //     Ok(result)
-    // }
+    pub async fn get_data_batch(
+        &self,
+        topic: &str,
+        pubkey: U256,
+        digests: &[Bytes32],
+    ) -> Result<Vec<DataWithTimestamp>> {
+        let pubkey_hex = pubkey.to_hex();
+        let digests_hex: Vec<String> = digests.iter().map(|d| d.to_hex()).collect();
+        let records = sqlx::query!(
+            r#"
+            SELECT data, timestamp
+            FROM historical_data
+            WHERE topic = $1 AND pubkey = $2 AND digest = ANY($3)
+            "#,
+            topic,
+            pubkey_hex,
+            &digests_hex
+        )
+        .fetch_all(&self.pool)
+        .await?;
 
-    // pub async fn get_data_sequence(
-    //     &self,
-    //     data_type: DataType,
-    //     pubkey: U256,
-    //     cursor: &MetaDataCursor,
-    // ) -> Result<(Vec<DataWithMetaData>, MetaDataCursorResponse)> {
-    //     let pubkey_hex = pubkey.to_hex();
-    //     let actual_limit = cursor.limit.unwrap_or(MAX_BATCH_SIZE as u32) as i64;
+        let result: Vec<DataWithTimestamp> = records
+            .into_iter()
+            .map(|r| DataWithTimestamp {
+                data: r.data,
+                timestamp: r.timestamp as u64,
+            })
+            .collect();
 
-    //     let result: Vec<DataWithMetaData> = match cursor.order {
-    //         CursorOrder::Asc => {
-    //             let cursor_meta = cursor.cursor.clone().unwrap_or_default();
-    //             sqlx::query!(
-    //                 r#"
-    //         SELECT uuid, encrypted_data
-    //         FROM encrypted_data
-    //         WHERE data_type = $1
-    //         AND pubkey = $2
-    //         AND uuid > $3
-    //         ORDER BY uuid ASC
-    //         LIMIT $4
-    //     "#,
-    //                 data_type as i32,
-    //                 pubkey_hex,
-    //                 cursor_meta.uuid,
-    //                 actual_limit + 1
-    //             )
-    //             .fetch_all(&self.pool)
-    //             .await?
-    //             .into_iter()
-    //             .map(|r| {
-    //                 let u = Uuid::parse_str(r.uuid.as_str()).unwrap_or_default();
-    //                 let meta = MetaData {
-    //                     uuid: r.uuid,
-    //                     timestamp: extract_timestamp_from_uuidv7(&u).0,
-    //                 };
-    //                 DataWithMetaData {
-    //                     meta,
-    //                     data: r.encrypted_data,
-    //                 }
-    //             })
-    //             .collect()
-    //         }
-    //         CursorOrder::Desc => {
-    //             let uuid = cursor
-    //                 .cursor
-    //                 .as_ref()
-    //                 .map(|c| c.uuid.clone())
-    //                 .unwrap_or(Uuid::max().to_string());
-    //             sqlx::query!(
-    //                 r#"
-    //         SELECT uuid, encrypted_data
-    //         FROM encrypted_data
-    //         WHERE data_type = $1
-    //         AND pubkey = $2
-    //         AND uuid < $3
-    //         ORDER BY uuid DESC
-    //         LIMIT $4
-    //     "#,
-    //                 data_type as i32,
-    //                 pubkey_hex,
-    //                 uuid,
-    //                 actual_limit + 1
-    //             )
-    //             .fetch_all(&self.pool)
-    //             .await?
-    //             .into_iter()
-    //             .map(|r| {
-    //                 let u = Uuid::parse_str(r.uuid.as_str()).unwrap_or_default();
-    //                 let meta = MetaData {
-    //                     uuid: r.uuid,
-    //                     timestamp: extract_timestamp_from_uuidv7(&u).0,
-    //                 };
-    //                 DataWithMetaData {
-    //                     meta,
-    //                     data: r.encrypted_data,
-    //                 }
-    //             })
-    //             .collect()
-    //         }
-    //     };
-    //     let has_more = result.len() > actual_limit as usize;
-    //     let result = result
-    //         .into_iter()
-    //         .take(actual_limit as usize)
-    //         .collect::<Vec<DataWithMetaData>>();
-    //     let next_cursor = result.last().map(|r| r.meta.clone());
-    //     let total_count = sqlx::query_scalar!(
-    //         r#"
-    //         SELECT COUNT(*) FROM encrypted_data
-    //         "#,
-    //     )
-    //     .fetch_one(&self.pool)
-    //     .await?
-    //     .unwrap_or(0) as u32;
-    //     let response_cursor = MetaDataCursorResponse {
-    //         next_cursor,
-    //         has_more,
-    //         total_count,
-    //     };
-    //     Ok((result, response_cursor))
-    // }
+        Ok(result)
+    }
+
+    pub async fn get_data_sequence(
+        &self,
+        topic: &str,
+        pubkey: U256,
+        cursor: &MetaDataCursor,
+    ) -> Result<(Vec<DataWithMetaData>, MetaDataCursorResponse)> {
+        let pubkey_hex = pubkey.to_hex();
+        let actual_limit = cursor.limit.unwrap_or(MAX_BATCH_SIZE as u32) as i64;
+
+        let result: Vec<DataWithMetaData> = match cursor.order {
+            CursorOrder::Asc => {
+                let cursor_meta = cursor.cursor.clone().unwrap_or_default();
+                sqlx::query!(
+                    r#"
+    SELECT digest, data, timestamp
+    FROM historical_data
+    WHERE topic = $1
+    AND pubkey = $2
+    AND (timestamp > $3 OR (timestamp = $3 AND digest > $4))
+    ORDER BY timestamp ASC, digest ASC
+    LIMIT $5
+    "#,
+                    topic,
+                    pubkey_hex,
+                    cursor_meta.timestamp as i64,
+                    cursor_meta.digest.to_hex(),
+                    actual_limit + 1
+                )
+                .fetch_all(&self.pool)
+                .await?
+                .into_iter()
+                .map(|r| {
+                    let meta = MetaData {
+                        timestamp: r.timestamp as u64,
+                        digest: Bytes32::from_hex(&r.digest).unwrap(),
+                    };
+                    DataWithMetaData { meta, data: r.data }
+                })
+                .collect()
+            }
+            CursorOrder::Desc => {
+                let (timestamp, digest) = cursor
+                    .cursor
+                    .as_ref()
+                    .map(|meta| (meta.timestamp as i64, meta.digest.to_hex()))
+                    .unwrap_or((i64::MAX, Bytes32::default().to_hex()));
+                sqlx::query!(
+                    r#"
+                    SELECT digest, data, timestamp
+                    FROM historical_data
+                     WHERE topic = $1
+                    AND pubkey = $2
+                    AND (timestamp < $3 OR (timestamp = $3 AND digest < $4))
+                    ORDER BY timestamp DESC, digest DESC
+                    LIMIT $5
+                "#,
+                    topic,
+                    pubkey_hex,
+                    timestamp,
+                    digest,
+                    actual_limit + 1
+                )
+                .fetch_all(&self.pool)
+                .await?
+                .into_iter()
+                .map(|r| {
+                    let meta = MetaData {
+                        digest: Bytes32::from_hex(&r.digest).unwrap(),
+                        timestamp: r.timestamp as u64,
+                    };
+                    DataWithMetaData { meta, data: r.data }
+                })
+                .collect()
+            }
+        };
+        let has_more = result.len() > actual_limit as usize;
+        let result = result
+            .into_iter()
+            .take(actual_limit as usize)
+            .collect::<Vec<DataWithMetaData>>();
+        let next_cursor = result.last().map(|r| r.meta.clone());
+        let total_count = sqlx::query_scalar!(
+            r#"
+            SELECT COUNT(*) FROM historical_data
+            "#,
+        )
+        .fetch_one(&self.pool)
+        .await?
+        .unwrap_or(0) as u32;
+        let response_cursor = MetaDataCursorResponse {
+            next_cursor,
+            has_more,
+            total_count,
+        };
+        Ok((result, response_cursor))
+    }
 }
 
 // #[cfg(test)]
