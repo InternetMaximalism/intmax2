bb57427f900e6f1737d6b41e33d10500d7969288
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-03-09 23:31:16 +0700

    extract rights

--- interfaces/src/data/topic.rs
+++ interfaces/src/data/topic.rs
@@ -10,3 +10,32 @@ pub fn topic_from_rights(read_rights: ReadRights, write_rights: WriteRights, nam
     };
     format!("v1/{}/{}", rw_rights, name)
 }
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum ExtractRightsError {
+    #[error("Invalid path")]
+    InvalidPath,
+
+    #[error("Invalid version")]
+    InvalidVersion,
+
+    #[error("rights parse error: {0}")]
+    RightsParseError(String),
+}
+
+pub fn extract_rights(topic: &str) -> Result<RWRights, ExtractRightsError> {
+    let parts: Vec<&str> = topic.split('/').collect();
+    if parts.len() > 2 {
+        return Err(ExtractRightsError::InvalidPath);
+    }
+    let version_part = parts[0];
+    if version_part != "v1" {
+        return Err(ExtractRightsError::InvalidVersion);
+    }
+    let rw_rights_part = parts[1];
+
+    let rw_rights: RWRights = rw_rights_part
+        .parse()
+        .map_err(ExtractRightsError::RightsParseError)?;
+    Ok(rw_rights)
+}
