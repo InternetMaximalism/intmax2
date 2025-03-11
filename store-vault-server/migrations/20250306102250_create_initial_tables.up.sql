fac39df150964f5e9dc6cbfc6f411d865dd8d0fc
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-03-11 19:17:33 +0800

    update query

--- store-vault-server/migrations/20250306102250_create_initial_tables.up.sql
+++ store-vault-server/migrations/20250306102250_create_initial_tables.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS snapshot_data (
     pubkey VARCHAR(66) NOT NULL,
-    digest VARCHAR(66) NOT NULL,
     topic VARCHAR(255) NOT NULL,
+    digest VARCHAR(66) NOT NULL,
     data BYTEA NOT NULL,
     timestamp BIGINT NOT NULL,
     UNIQUE (pubkey, topic)
