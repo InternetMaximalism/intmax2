c471f1624730ebead045f53ea6e3ebf8a1721af4
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-04-04 14:20:54 +0700

    update readme

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -22,7 +22,7 @@ use intmax2_zkp::{
 use num_bigint::BigUint;
 use serde::Deserialize;
 
-const MAX_BATCH_TRANSFER: usize = 5;
+const MAX_BATCH_TRANSFER: usize = 64;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
