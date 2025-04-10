6dc9f0019ac48a370381281fecee87342b65900c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-04-11 00:29:53 +0700

    add aml permitter

--- client-sdk/src/external_api/predicate.rs
+++ client-sdk/src/external_api/predicate.rs
@@ -1,10 +1,8 @@
 use ethers::{
-    abi::{Function, Param, ParamType, StateMutability, Token, Tokenizable},
-    contract::encode_function_data,
-    types::{Address as EtherAddress, Bytes, H256 as EtherH256, U256 as EtherU256},
+    abi::{Function, Param, ParamType, StateMutability, Token},
+    types::{Address as EtherAddress, H256 as EtherH256, U256 as EtherU256},
 };
 use intmax2_interfaces::api::error::ServerError;
-use intmax2_zkp::ethereum_types::{address::Address, bytes32::Bytes32, u256::U256};
 use serde::Deserialize;
 
 use crate::external_api::utils::query::post_request;
@@ -34,7 +32,7 @@ pub enum PermissionRequest {
 }
 
 impl PermissionRequest {
-    pub fn to_encoded_data(&self) -> Bytes {
+    pub fn to_encoded_data(&self) -> Vec<u8> {
         match self {
             PermissionRequest::Native {
                 recipient_salt_hash,
@@ -46,20 +44,18 @@ impl PermissionRequest {
                     inputs: vec![Param {
                         name: "recipientSaltHash".to_string(),
                         kind: ParamType::FixedBytes(32),
-                        internal_type: Some("bytes32".to_string()),
+                        internal_type: None,
                     }],
                     constant: None,
                     outputs: vec![],
                     state_mutability: StateMutability::NonPayable,
                 };
 
-                encode_function_data(
-                    &function,
-                    [Token::FixedBytes(
+                function
+                    .encode_input(&[Token::FixedBytes(
                         recipient_salt_hash.to_fixed_bytes().to_vec(),
-                    )],
-                )
-                .unwrap()
+                    )])
+                    .unwrap()
             }
             PermissionRequest::ERC20 {
                 token_address,
@@ -91,15 +87,13 @@ impl PermissionRequest {
                     state_mutability: StateMutability::NonPayable,
                 };
 
-                encode_function_data(
-                    &function,
-                    [
+                function
+                    .encode_input(&[
                         Token::Address(*token_address),
                         Token::FixedBytes(recipient_salt_hash.to_fixed_bytes().to_vec()),
                         Token::Uint(*amount),
-                    ],
-                )
-                .unwrap()
+                    ])
+                    .unwrap()
             }
             PermissionRequest::ERC721 {
                 token_address,
@@ -131,15 +125,13 @@ impl PermissionRequest {
                     state_mutability: StateMutability::NonPayable,
                 };
 
-                encode_function_data(
-                    &function,
-                    [
+                function
+                    .encode_input(&[
                         Token::Address(*token_address),
                         Token::FixedBytes(recipient_salt_hash.to_fixed_bytes().to_vec()),
                         Token::Uint(*token_id),
-                    ],
-                )
-                .unwrap()
+                    ])
+                    .unwrap()
             }
             PermissionRequest::ERC1155 {
                 token_address,
@@ -177,16 +169,14 @@ impl PermissionRequest {
                     state_mutability: StateMutability::NonPayable,
                 };
 
-                encode_function_data(
-                    &function,
-                    [
+                function
+                    .encode_input(&[
                         Token::Address(*token_address),
                         Token::FixedBytes(recipient_salt_hash.to_fixed_bytes().to_vec()),
                         Token::Uint(*token_id),
                         Token::Uint(*amount),
-                    ],
-                )
-                .unwrap()
+                    ])
+                    .unwrap()
             }
         }
     }
@@ -198,37 +188,33 @@ pub struct PredicateClient {
 }
 
 impl PredicateClient {
+    pub fn new(base_url: String) -> Self {
+        PredicateClient { base_url }
+    }
+
     pub async fn get_deposit_permission(
         &self,
-        from: Address,
-        to: Address,
-        value: U256,
+        from: EtherAddress,
+        to: EtherAddress,
+        value: EtherU256,
         request: PermissionRequest,
     ) -> Result<Vec<u8>, ServerError> {
         let encoded_data = request.to_encoded_data();
-        let body = serde_json::json!({
-            "from": from.to_string(),
-            "to": to.to_string(),
-            "data": "0x".to_string() + &hex::encode(encoded_data),
-            "msg_value": value.to_string(),
-        });
-        let response: PredicateResponse =
-            post_request(&self.base_url, "/v1/predicate/evaluate-policy", Some(&body)).await?;
-        Ok(encode_predicate_message(response))
+        self.get_permission(from, to, value, &encoded_data).await
     }
 
     async fn get_permission(
         &self,
-        from: Address,
-        to: Address,
-        value: U256,
+        from: EtherAddress,
+        to: EtherAddress,
+        value: EtherU256,
         encoded_data: &[u8],
     ) -> Result<Vec<u8>, ServerError> {
         let body = serde_json::json!({
-            "from": from.to_string(),
-            "to": to.to_string(),
+            "from": format!("{:?}", from),
+            "to": format!("{:?}", to),
             "data": "0x".to_string() + &hex::encode(encoded_data),
-            "msg_value": value.to_string(),
+            "msg_value":format!("{:?}", value),
         });
         let response: PredicateResponse =
             post_request(&self.base_url, "/v1/predicate/evaluate-policy", Some(&body)).await?;
@@ -266,44 +252,5 @@ fn encode_predicate_message(message: PredicateResponse) -> Vec<u8> {
                 .collect(),
         ),
     ]);
-    ethers::abi::encode(&[tokens]).into()
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-    use intmax2_zkp::ethereum_types::{address::Address, u32limb_trait::U32LimbTrait};
-
-    #[test]
-    fn test_encode_request() {
-        let request = PermissionRequest::Native {
-            recipient_salt_hash: EtherH256::zero(),
-            amount: EtherU256::zero(),
-        };
-        let _encoded_data = request.to_encoded_data();
-    }
-
-    #[tokio::test]
-    async fn test_get_deposit_permission() {
-        let client = PredicateClient {
-            base_url: "https://sandbox.mainnet.api.predicate.intmax.xyz".to_string(),
-        };
-        let from = Address::zero();
-        let to = Address::from_hex("0x026bC6D4deec82cFF6450bc1D5bEb3B62724217b").unwrap();
-        let value = U256::zero();
-        let result = client
-            .get_deposit_permission(
-                from,
-                to,
-                value,
-                PermissionRequest::Native {
-                    recipient_salt_hash: EtherH256::zero(),
-                    amount: EtherU256::zero(),
-                },
-            )
-            .await
-            .unwrap();
-        assert!(!result.is_empty());
-    }
+    ethers::abi::encode(&[tokens])
 }
