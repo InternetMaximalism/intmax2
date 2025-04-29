5374d98a4fc50087961670bbcee079b12949d153
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-04-29 16:44:09 +0700

    block builder

--- block-builder/src/app/jobs.rs
+++ block-builder/src/app/jobs.rs
@@ -118,6 +118,7 @@ impl BlockBuilder {
         log::info!("Posting block: {}", block_post_task.block_id);
         match post_block(
             self.config.block_builder_private_key,
+            self.config.gas_limit_for_block_post,
             self.config.eth_allowance_for_block,
             &self.rollup_contract,
             &self.validity_prover_client,
