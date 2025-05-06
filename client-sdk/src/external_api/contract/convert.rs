f29f50a3ea5e315e702137c4815e7fdf4b28066c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-05-07 01:13:20 +0700

    wip: liquidity

--- client-sdk/src/external_api/contract/convert.rs
+++ client-sdk/src/external_api/contract/convert.rs
@@ -1,4 +1,4 @@
-use alloy::primitives::{Address as AlloyAddress, B128, B256, U256 as AlloyU256};
+use alloy::primitives::{Address as AlloyAddress, TxHash, B128, B256, U256 as AlloyU256};
 use intmax2_zkp::ethereum_types::{
     address::Address as IntmaxAddress, bytes16::Bytes16, bytes32::Bytes32,
     u256::U256 as IntmaxU256, u32limb_trait::U32LimbTrait,
@@ -21,6 +21,10 @@ pub fn convert_address_to_intmax(input: AlloyAddress) -> IntmaxAddress {
 }
 
 pub fn convert_b256_to_bytes32(input: B256) -> Bytes32 {
+    Bytes32::from_bytes_be(&input.0).unwrap()
+}
+
+pub fn convert_tx_hash_to_bytes32(input: TxHash) -> Bytes32 {
     Bytes32::from_bytes_be(&input.0).unwrap()
 }
 
