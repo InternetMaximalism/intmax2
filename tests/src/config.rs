38d44c38e0f110e255e35344fcaf050e7cba7635
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-05-11 13:22:28 +0700

    wip

---
+++ tests/src/config.rs
@@ -0,0 +1,4 @@
+pub struct TestConfig {
+    pub deposit_check_interval: u64,
+    pub deposit_check_retries: u64,
+}
--- tests/src/deposit_withdrawal.rs
+++ tests/src/deposit_withdrawal.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use alloy::{
     primitives::{Address as AlloyAddress, B256, U256 as AlloyU256},
     providers::Provider,
@@ -11,9 +13,15 @@ use intmax2_client_sdk::{
     },
 };
 use intmax2_interfaces::data::deposit_data::TokenType;
-use intmax2_zkp::ethereum_types::address::Address;
+use intmax2_zkp::{
+    common::signature_content::key_set::KeySet,
+    ethereum_types::{address::Address, u256::U256},
+};
+
+use crate::config::TestConfig;
 
 pub async fn single_deposit_withdrawal(
+    config: &TestConfig,
     client: &Client,
     eth_private_key: B256,
 ) -> anyhow::Result<()> {
@@ -59,14 +67,71 @@ pub async fn single_deposit_withdrawal(
         .deposit_native(
             eth_private_key,
             None,
-            deposit_result.deposit_data.pubkey_salt_hash,
-            deposit_result.deposit_data.amount,
+            deposit_data.pubkey_salt_hash,
+            deposit_data.amount,
             &aml_permission,
             &eligibility_permission,
         )
         .await?;
 
-    // Wait for the deposit to be confirmed
+    // Wait for the deposit to be synced to the validity prover
+    let mut retries = 0;
+    loop {
+        if retries >= config.deposit_check_retries {
+            return Err(anyhow::anyhow!("Deposit is not synced to validity prover"));
+        }
+        let deposit_info = client
+            .validity_prover
+            .get_deposit_info(deposit_data.pubkey_salt_hash)
+            .await?;
+        if deposit_info.is_some() {
+            break;
+        }
+        log::warn!("Deposit is not synced to validity prover, retrying...");
+        tokio::time::sleep(Duration::from_secs(config.deposit_check_interval)).await;
+        retries += 1;
+    }
+    log::info!("Deposit is synced to validity prover");
+
+    // Wait for the deposit to be relayed to the L2
+    let mut retries = 0;
+    loop {
+        if retries >= config.deposit_check_retries {
+            return Err(anyhow::anyhow!("Deposit is not synced to validity prover"));
+        }
+        let deposit_info = client
+            .validity_prover
+            .get_deposit_info(deposit_data.pubkey_salt_hash)
+            .await?
+            .ok_or(anyhow::anyhow!(
+                "Deposit is disappeared from validity prover"
+            ))?;
+        if deposit_info.block_number.is_some() {
+            break;
+        }
+        log::warn!("Deposit is not synced to L2, retrying...");
+        tokio::time::sleep(Duration::from_secs(config.deposit_check_interval)).await;
+        retries += 1;
+    }
+    log::info!("Deposit is relayed to L2");
+
+    // sync balance
+    client.sync(key).await?;
+    log::info!("Synced balance");
+
+    let intmax_balance = get_balance_on_intmax(client, key).await?;
+    if intmax_balance < deposit_data.amount {
+        return Err(anyhow::anyhow!(
+            "Deposit is not reflected in the balance: {}",
+            intmax_balance
+        ));
+    }
+
+    // withdraw
+
+    let transfer_fee = client
+        .quote_transfer_fee(block_builder_url, pubkey, fee_token_index)
+        .await?;
 
     Ok(())
 }
@@ -89,4 +154,10 @@ async fn calculate_balance_with_gas_deduction(
     }
     let new_balance = balance - gas_fee * AlloyU256::from(multiplier);
     Ok(new_balance)
+}
+
+async fn get_balance_on_intmax(client: &Client, key: KeySet) -> anyhow::Result<U256> {
+    let balance = client.get_user_data(key).await?.balances();
+    let eth_balance = balance.0.get(&0).map_or(U256::default(), |b| b.amount);
+    Ok(eth_balance)
 }
