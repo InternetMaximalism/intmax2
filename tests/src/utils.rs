01c4510499afe8a14a5eaedd7e43be778812a322
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-05-12 10:48:17 +0700

    chore: url validation of block builder

--- tests/src/utils.rs
+++ tests/src/utils.rs
@@ -14,6 +14,7 @@ use intmax2_zkp::{
     common::signature_content::key_set::KeySet,
     ethereum_types::{u256::U256, u32limb_trait::U32LimbTrait},
 };
+use reqwest::Url;
 
 pub async fn calculate_balance_with_gas_deduction(
     provider: &NormalProvider,
@@ -47,6 +48,9 @@ pub async fn get_block_builder_url(indexer_url: &str) -> anyhow::Result<String> 
     if block_builder_info.is_empty() {
         return Err(anyhow::anyhow!("Block builder info is empty"));
     }
+    let url = block_builder_info.first().unwrap().url.clone();
+    let _ =
+        Url::parse(&url).map_err(|_| anyhow::anyhow!("Malformed block builder URL, {}", url))?;
     Ok(block_builder_info.first().unwrap().url.clone())
 }
 
