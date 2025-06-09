ffeb4e5abf016f770f6e37ac0518c8e83a647fba
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-06-09 13:45:05 +0700

    wip: fix misc

--- client-sdk/src/client/misc/payment_memo.rs
+++ client-sdk/src/client/misc/payment_memo.rs
@@ -11,8 +11,9 @@ use intmax2_interfaces::{
         topic::topic_from_rights,
         transfer_data::TransferData,
     },
+    utils::key::PrivateKey,
 };
-use intmax2_zkp::{common::signature_content::key_set::KeySet, ethereum_types::bytes32::Bytes32};
+use intmax2_zkp::ethereum_types::bytes32::Bytes32;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 pub fn payment_memo_topic(name: &str) -> String {
@@ -44,23 +45,25 @@ impl BlsEncryption for PaymentMemo {
 
 pub async fn save_payment_memo<M: Default + Clone + Serialize + DeserializeOwned>(
     store_vault_server: &dyn StoreVaultClientInterface,
-    key: KeySet,
+    view_priv: PrivateKey,
     memo_name: &str,
     payment_memo: &PaymentMemo,
 ) -> Result<Bytes32, ClientError> {
     let topic = payment_memo_topic(memo_name);
     let entry = SaveDataEntry {
         topic,
-        pubkey: key.pubkey,
-        data: payment_memo.encrypt(key.pubkey, Some(key))?,
+        pubkey: view_priv.to_public_key().0,
+        data: payment_memo.encrypt(view_priv.to_public_key(), Some(view_priv))?,
     };
-    let digests = store_vault_server.save_data_batch(key, &[entry]).await?;
+    let digests = store_vault_server
+        .save_data_batch(view_priv, &[entry])
+        .await?;
     Ok(digests[0])
 }
 
 pub async fn get_all_payment_memos(
     store_vault_server: &dyn StoreVaultClientInterface,
-    key: KeySet,
+    view_priv: PrivateKey,
     memo_name: &str,
 ) -> Result<Vec<PaymentMemo>, SyncError> {
     let topic = payment_memo_topic(memo_name);
@@ -69,7 +72,7 @@ pub async fn get_all_payment_memos(
     loop {
         let (encrypted_memos_partial, cursor_response) = store_vault_server
             .get_data_sequence(
-                key,
+                view_priv,
                 &topic,
                 &MetaDataCursor {
                     cursor: cursor.clone(),
@@ -88,7 +91,8 @@ pub async fn get_all_payment_memos(
 
     let mut memos = Vec::new();
     for encrypted_memo in encrypted_memos {
-        let memo = PaymentMemo::decrypt(key, None, &encrypted_memo.data)?;
+        // todo: specify sender view pub
+        let memo = PaymentMemo::decrypt(view_priv, None, &encrypted_memo.data)?;
         memos.push(memo);
     }
 
